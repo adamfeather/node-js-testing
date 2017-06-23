0e0cba7b5350d43c9ea4a5cf396c5bf4b4bc2293
Adam Feather <FEATHEA@domesticandgeneral.com>
2017-06-23 10:59:29 +0100

    test1

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 var sql = require("mssql");
+var express = require("express");
+var app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.set('port', process.env.PORT || 8080);
+
+app.get('/', function (req, res) {
+    res.json({ test: "test" });
+});
+
+app.listen(app.get('port'));
